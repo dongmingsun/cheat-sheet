d5763e95e69666a84824e86da9de360a95d73150
dongmingsun <dongming.sun@outlook.com>
2018-06-25 13:58:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## brew
+`brew info <formula>`
+`brew switch <formula> <version>`
+
+## c++
+`std::boolalpha`
+
+## conda
+`conda info --envs`
+`conda env remove -n caffe2`
+
+## Decompress
+`tar -xvzf <file_name.tar.gz>`
+
+## OpenCV
+type2str
+
---
+++ cc/opencv.cc
@@ -0,0 +1,38 @@
+std::string type2str(cv::Mat img) {
+  int type = img.type();
+  std::string r;
+  int depth = type & CV_MAT_DEPTH_MASK;
+  int chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch (depth) {
+  case CV_8U:
+    r = "8U";
+    break;
+  case CV_8S:
+    r = "8S";
+    break;
+  case CV_16U:
+    r = "16U";
+    break;
+  case CV_16S:
+    r = "16S";
+    break;
+  case CV_32S:
+    r = "32S";
+    break;
+  case CV_32F:
+    r = "32F";
+    break;
+  case CV_64F:
+    r = "64F";
+    break;
+  default:
+    r = "User";
+    break;
+  }
+  r += "C";
+  r += (chans + '0');
+  std::cout << r << std::endl;
+
+  return r;
+}
